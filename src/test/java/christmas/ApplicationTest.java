6331c75a93987e17673e0656d7089262cb495a16
PJH <parkjh0858@gmail.com>
2023-11-10 01:28:14 +0900

    test: 카테고리별 메뉴 생성 테스트 추가

--- src/test/java/christmas/ApplicationTest.java
+++ src/test/java/christmas/ApplicationTest.java
@@ -4,10 +4,28 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import menu.Food;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class ApplicationTest extends NsTest {
     private static final String LINE_SEPARATOR = System.lineSeparator();
+
+    @ParameterizedTest
+    @CsvSource(value = {"애피타이저:타파스,양송이수프,시저샐러드", "메인:해산물파스타,티본스테이크,크리스마스파스타,바비큐립"
+    ,"디저트:아이스크림,초코케이크", "음료:제로콜라,레드와인,샴페인"}, delimiter = ':')
+    @DisplayName("카테고리별로 메뉴가 생성되는지 확인한다.")
+    void menu(String category, String foods) {
+        String[] data = foods.split(",");
+        List<String> foodItems = new ArrayList<>(Arrays.asList(data));
+
+        assertThat(foodItems.containsAll(Food.getFoodsByCategory(category))).isTrue();
+    }
 
     @Test
     void 모든_타이틀_출력() {
