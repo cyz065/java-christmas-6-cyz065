5c60c41af24bae2f926ccb613dcfb97305835767
PJH <parkjh0858@gmail.com>
2023-11-10 23:02:04 +0900

    feat: 입,출력을 위한 상수 클래스 추가

---
+++ src/main/java/constant/Constant.java
@@ -0,0 +1,6 @@
+package constant;
+
+public class Constant {
+    public static final String TITLE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    public static final String INPUT_DAY = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+}
