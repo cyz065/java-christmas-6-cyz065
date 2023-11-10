33333692db272b06affdb7cf741904e89de6a78a
PJH <parkjh0858@gmail.com>
2023-11-11 01:42:12 +0900

    refactor: 음료만 주문, 20개 이상 주문 검증 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,13 +1,15 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+
 import constant.Constant;
 import constant.Exception;
-import java.io.IOException;
-import java.util.ArrayList;
+
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+
 import menu.Menu;
 
 public class InputView {
@@ -35,7 +37,6 @@ public class InputView {
         try {
             int day = Integer.parseInt(input);
             checkDay(day);
-
             return day;
         } catch (NumberFormatException exception) {
             throw new IllegalArgumentException(Exception.DAY_FORMAT_ERROR);
@@ -48,31 +49,61 @@ public class InputView {
         }
     }
 
-    public static Map<String, Integer> setOrder(String input) {
-        Map<String, Integer> menu = new HashMap<>();
-        String[] menuPair = input.split(",");
-
-        for (String pair : menuPair) {
-            try {
-                String[] item = pair.split("-");
-                checkInputOrder(item);
-
-                menu.put(item[0], Integer.parseInt(item[1]));
-            } catch (IllegalArgumentException exception) {
-                throw new IllegalArgumentException(exception.getMessage());
-            }
+    public static Map<String, Integer> setOrder(String input) throws IllegalArgumentException {
+        // order: {아이템, 수량} ex) {레드와인 = 1}
+        Map<String, Integer> order = new HashMap<>();
+        String[] orderPair = input.split(",");
+        try {
+            fillOrder(orderPair, order);
+            checkOnlyDrink(order);
+            checkTotalCount(order);
+        } catch (IllegalArgumentException exception) {
+            throw new IllegalArgumentException(exception.getMessage());
         }
-
-        return menu;
+        return order;
     }
 
-    private static void checkInputOrder(String[] pair) throws IllegalArgumentException {
+    private static void fillOrder(String[] orderPair, Map<String, Integer> order) throws IllegalArgumentException {
+        for (String pair : orderPair) {
+            String[] item = pair.split("-");
+            try {
+                checkInputOrder(item, order);
+                order.put(item[0], Integer.parseInt(item[1]));
+            } catch (IllegalArgumentException exception) {
+                throw new IllegalArgumentException(Exception.MENU_ERROR);
+            }
+        }
+    }
+
+    private static void checkInputOrder(String[] pair, Map<String, Integer> order) throws IllegalArgumentException {
         try {
             checkLength(pair);
-            //checkItem(pair[0]);
-            checkCount(pair[1]);
-
+            checkMenuContainsItem(pair[0]);
+            checkItemDuplication(pair[0], order);
+            checkValidCount(pair[1]);
         } catch (IllegalArgumentException exception) {
+            throw new IllegalArgumentException(Exception.MENU_ERROR);
+        }
+    }
+
+    private static void checkOnlyDrink(Map<String, Integer> order) throws IllegalArgumentException {
+        Set<String> category = new HashSet<>();
+        for (String item : order.keySet()) {
+            category.add(Menu.getItemCategory(item));
+        }
+
+        if (category.size() == 1 && category.contains(Constant.DRINK)) {
+            throw new IllegalArgumentException(Exception.MENU_ERROR);
+        }
+    }
+
+    private static void checkTotalCount(Map<String, Integer> order) {
+        int total = 0;
+        for (String item : order.keySet()) {
+            total += order.get(item);
+        }
+
+        if (total > 20) {
             throw new IllegalArgumentException(Exception.MENU_ERROR);
         }
     }
@@ -83,19 +114,24 @@ public class InputView {
         }
     }
 
-    /*
-    private static void checkItem(String item) throws IllegalArgumentException {
+    private static void checkMenuContainsItem(String item) throws IllegalArgumentException {
         if (!Menu.containsItem(item)) {
             throw new IllegalArgumentException(Exception.MENU_ERROR);
         }
-    }*/
+    }
 
-    private static void checkCount(String input) throws IllegalArgumentException {
-        int count = checkValid(input);
+    private static void checkItemDuplication(String item, Map<String, Integer> order) throws IllegalArgumentException {
+        if (order.getOrDefault(item, null) != null) {
+            throw new IllegalArgumentException(Exception.MENU_ERROR);
+        }
+    }
+
+    private static void checkValidCount(String input) throws IllegalArgumentException {
+        int count = convertToInt(input);
         checkRange(count);
     }
 
-    private static int checkValid(String input) throws IllegalArgumentException {
+    private static int convertToInt(String input) throws IllegalArgumentException {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException exception) {
