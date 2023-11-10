3aab4faca5098c70bcc4637254be67833bc43863
PJH <parkjh0858@gmail.com>
2023-11-11 01:40:55 +0900

    feat: 카테고리 상수 추가

--- src/main/java/constant/Constant.java
+++ src/main/java/constant/Constant.java
@@ -1,6 +1,10 @@
 package constant;
 
 public class Constant {
+    public static final String APPETIZER = "애피타이저";
+    public static final String MAIN = "메인";
+    public static final String DESSERT = "디저트";
+    public static final String DRINK = "음료";
     public static final String TITLE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     public static final String INPUT_DAY = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     public static final String INPUT_ORDER = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
