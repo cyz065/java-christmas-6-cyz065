793a4e941867151701eabc593a70f249b3e7a4cd
PJH <parkjh0858@gmail.com>
2023-11-11 00:24:19 +0900

    feat: 출력 문구 상수 추가

--- src/main/java/constant/Constant.java
+++ src/main/java/constant/Constant.java
@@ -3,4 +3,13 @@ package constant;
 public class Constant {
     public static final String TITLE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     public static final String INPUT_DAY = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public static final String INPUT_ORDER = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    public static final String EVENT_PREVIEW = "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    public static final String PRINT_ORDER = "<주문 메뉴>";
+
+
+
+
+
+
 }
