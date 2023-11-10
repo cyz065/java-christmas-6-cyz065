dc46b134ff8b27e9e6556cec0edde07bcda4cef5
PJH <parkjh0858@gmail.com>
2023-11-11 00:23:25 +0900

    feat: 사용자 주문 입력 및 검증 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,15 +1,30 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import constant.Constant;
 import constant.Exception;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import menu.Menu;
 
 public class InputView {
-
     public static int inputDay() {
         while (true) {
             try {
                 return setDay(Console.readLine());
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+    }
+
+    public static Map<String, Integer> inputOrder() {
+        while (true) {
+            try {
+                return setOrder(Console.readLine());
             } catch (IllegalArgumentException exception) {
                 System.out.println(exception.getMessage());
             }
@@ -27,12 +42,71 @@ public class InputView {
         }
     }
 
-    public static void checkDay(int day) throws IllegalArgumentException {
+    private static void checkDay(int day) throws IllegalArgumentException {
         if (day < 1 || day > 31) {
             throw new IllegalArgumentException(Exception.DAY_RANGE_ERROR);
         }
     }
 
+    public static Map<String, Integer> setOrder(String input) {
+        Map<String, Integer> menu = new HashMap<>();
+        String[] menuPair = input.split(",");
 
+        for (String pair : menuPair) {
+            try {
+                String[] item = pair.split("-");
+                checkInputOrder(item);
 
+                menu.put(item[0], Integer.parseInt(item[1]));
+            } catch (IllegalArgumentException exception) {
+                throw new IllegalArgumentException(exception.getMessage());
+            }
+        }
+
+        return menu;
+    }
+
+    private static void checkInputOrder(String[] pair) throws IllegalArgumentException {
+        try {
+            checkLength(pair);
+            checkItem(pair[0]);
+            checkCount(pair[1]);
+
+        } catch (IllegalArgumentException exception) {
+            throw new IllegalArgumentException(Exception.MENU_ERROR);
+        }
+    }
+
+    private static void checkLength(String[] pair) throws IllegalArgumentException {
+        if (pair.length != 2) {
+            throw new IllegalArgumentException(Exception.MENU_ERROR);
+        }
+    }
+
+    private static void checkItem(String item) throws IllegalArgumentException {
+        if (!Menu.containsItem(item)) {
+            throw new IllegalArgumentException(Exception.MENU_ERROR);
+        }
+    }
+
+    private static int checkCount(String input) throws IllegalArgumentException {
+        int count = checkValid(input);
+        checkRange(count);
+
+        return count;
+    }
+
+    private static int checkValid(String input) throws IllegalArgumentException {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(Exception.MENU_ERROR);
+        }
+    }
+
+    private static void checkRange(int count) throws IllegalArgumentException {
+        if (count <= 0) {
+            throw new IllegalArgumentException(Exception.MENU_ERROR);
+        }
+    }
 }
