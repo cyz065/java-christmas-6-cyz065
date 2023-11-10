7105374458c133c44b838d6f5a7710423ebfb275
PJH <parkjh0858@gmail.com>
2023-11-11 00:24:00 +0900

    feat: 메뉴 오류 상수 추가

--- src/main/java/constant/Exception.java
+++ src/main/java/constant/Exception.java
@@ -3,5 +3,6 @@ package constant;
 public class Exception {
     public static final String DAY_RANGE_ERROR = "[ERROR] 유효하지 않은 날짜 입니다. 다시 입력해 주세요.";
     public static final String DAY_FORMAT_ERROR = "[ERROR] 날짜는 숫자만 입력 가능합니다. 다시 입력해 주세요.";
+    public static final String MENU_ERROR = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
 
 }
