7e7735acb5c9986855a94f3c84141ca33ed876b1
PJH <parkjh0858@gmail.com>
2023-11-10 23:02:17 +0900

    feat: 예외 메시지 관리를 위한 클래스 추가

---
+++ src/main/java/constant/Exception.java
@@ -0,0 +1,7 @@
+package constant;
+
+public class Exception {
+    public static final String DAY_RANGE_ERROR = "[ERROR] 유효하지 않은 날짜 입니다. 다시 입력해 주세요.";
+    public static final String DAY_FORMAT_ERROR = "[ERROR] 날짜는 숫자만 입력 가능합니다. 다시 입력해 주세요.";
+
+}
