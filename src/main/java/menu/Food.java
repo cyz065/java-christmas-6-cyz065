24684f44c9d85336fc60123ae6ab7e0df9969fd7
PJH <parkjh0858@gmail.com>
2023-11-11 01:41:36 +0900

    refactor: 하드코딩된 카테고리 상수로 변경

--- src/main/java/menu/Food.java
+++ src/main/java/menu/Food.java
@@ -1,13 +1,14 @@
 package menu;
 
+import constant.Constant;
 import java.util.ArrayList;
 import java.util.List;
 
 public enum Food {
-    MUSHROOM_SOUP("애피타이저", "양송이수프", 6_000), TAPAS("애피타이저", "타파스", 5_500), CAESAR_SALAD("애피타이저","시저샐러드", 8_000),
-    T_BONE_STEAK("메인","티본스테이크", 55_000), BARBEQUE_RIB("메인","바비큐립", 54_000), SEA_FOOD_PASTA("메인","해산물파스타", 35_000), CHRISTMAS_PASTA("메인","크리스마스파스타", 25_000),
-    CHOCOLATE_CAKE("디저트","초코케이크", 15_000), ICE_CREAM("디저트","아이스크림", 5_000),
-    ZERO_COKE("음료","제로콜라", 3_000), RED_WINE("음료","레드와인", 60_000), CHAMPAGNE("음료","샴페인", 25_000);
+    MUSHROOM_SOUP(Constant.APPETIZER, "양송이수프", 6_000), TAPAS(Constant.APPETIZER, "타파스", 5_500), CAESAR_SALAD(Constant.APPETIZER,"시저샐러드", 8_000),
+    T_BONE_STEAK(Constant.MAIN,"티본스테이크", 55_000), BARBEQUE_RIB(Constant.MAIN,"바비큐립", 54_000), SEA_FOOD_PASTA(Constant.MAIN,"해산물파스타", 35_000), CHRISTMAS_PASTA("메인","크리스마스파스타", 25_000),
+    CHOCOLATE_CAKE(Constant.DESSERT,"초코케이크", 15_000), ICE_CREAM(Constant.DESSERT,"아이스크림", 5_000),
+    ZERO_COKE(Constant.DRINK,"제로콜라", 3_000), RED_WINE(Constant.DRINK,"레드와인", 60_000), CHAMPAGNE(Constant.DRINK,"샴페인", 25_000);
 
     private String category;
     private String name;
