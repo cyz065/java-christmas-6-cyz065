62c79ef61d5d996b8d3f0735aeeca6b098eabd5e
PJH <parkjh0858@gmail.com>
2023-11-11 16:59:26 +0900

    refactor: 메뉴관련 주석 추가

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -14,6 +14,7 @@ public class Menu {
         itemWithCategory = setMenuByItem();
     }
 
+    // 카테고리별 음식들 나열 ex) 애피타이저 = [양송이수프, 타파스, 시저샐러드]
     public static Map<String, List<String>> setMenuByCategory() {
         Map<String, List<String>> menu = new HashMap<>();
 
@@ -24,6 +25,7 @@ public class Menu {
         return menu;
     }
 
+    // 음식 별 카테고리 나열 ex) 양송이수프 - 애피타이저, 티본스테이크 - 메인
     public static Map<String, String> setMenuByItem() {
         Map<String, String> menu = new HashMap<>();
 
