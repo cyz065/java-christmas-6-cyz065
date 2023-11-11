7c1700f85015a9e784a68297390ea000f8d92e9e
PJH <parkjh0858@gmail.com>
2023-11-11 16:58:27 +0900

    refactor: 할인관련 상수 추가

--- src/main/java/constant/Constant.java
+++ src/main/java/constant/Constant.java
@@ -10,6 +10,8 @@ public class Constant {
     public static final String INPUT_ORDER = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
     public static final String EVENT_PREVIEW = "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     public static final String PRINT_ORDER = "<주문 메뉴>";
+    public static final int TARGET_PRICE = 10_000;
+    public static final int DAY_DISCOUNT = 2_023;
 
 
 
