feb22d03fc80410b7929baba2a63555dd75aa0c5
PJH <parkjh0858@gmail.com>
2023-11-12 16:26:55 +0900

    feat: 출력을 위한 상수 추가

--- src/main/java/constant/Constant.java
+++ src/main/java/constant/Constant.java
@@ -1,15 +1,24 @@
 package constant;
 
 public class Constant {
+    public static final int EVENT_MONTH = 12;
     public static final String APPETIZER = "애피타이저";
     public static final String MAIN = "메인";
     public static final String DESSERT = "디저트";
     public static final String DRINK = "음료";
-    public static final String TITLE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
-    public static final String INPUT_DAY = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public static final String TITLE = "안녕하세요! 우테코 식당 " + EVENT_MONTH + "월 이벤트 플래너입니다.";
+    public static final String INPUT_DAY = EVENT_MONTH + "월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     public static final String INPUT_ORDER = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
     public static final String EVENT_PREVIEW = "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     public static final String PRINT_ORDER = "<주문 메뉴>";
+    public static final String PRINT_TOTAL_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
+    public static final String GIFT_MENU = "<증정 메뉴>";
+    public static final String BENEFIT_HISTORY = "<혜택 내역>";
+    public static final String TOTAL_BENEFIT = "<총혜택 금액>";
+    public static final String EXPECTED_PRICE = "<할인 후 예상 결제 금액>";
+    public static final String EVENT_BADGE = "<" + EVENT_MONTH + "월 이벤트 배지>";
+    public static final String GIFT = "샴페인 1개";
+    public static final String NOTHING = "없음";
     public static final int TARGET_PRICE = 10_000;
     public static final int DAY_DISCOUNT = 2_023;
     public static final int CHRISTMAS = 25;
@@ -17,10 +26,9 @@ public class Constant {
     public static final int CHRISTMAS_DISCOUNT = 100;
     public static final int NO_DISCOUNT = 0;
     public static final int GIFT_TARGET_PRICE = 120_000;
-
-
-
-
-
-
+    public static final String CHRISTMAS_D_DAY = "크리스마스 디데이 할인";
+    public static final String WEEKDAY = "평일 할인";
+    public static final String WEEKEND = "주말 할인";
+    public static final String SPECIAL = "특별 할인";
+    public static final String GIFT_EVENT = "증정 이벤트";
 }
