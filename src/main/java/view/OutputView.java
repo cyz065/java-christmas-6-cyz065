f16375c7374f93418a3b950916404e1711e44bbc
PJH <parkjh0858@gmail.com>
2023-11-13 22:03:10 +0900

    refactor: 출력 메소드 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,8 +2,12 @@ package view;
 
 
 import constant.Constant;
+import event.Badge;
+import event.Discount;
 import java.text.DecimalFormat;
 import java.util.Map;
+import menu.Food;
+import order.Order;
 
 public class OutputView {
     static DecimalFormat moneyFormat = new DecimalFormat("###,###");
@@ -17,56 +21,83 @@ public class OutputView {
         System.out.println();
     }
 
-    public static void printMenu(Map<String, Integer> order) {
+    public static void printMenu(Order order) {
         System.out.println(Constant.PRINT_ORDER);
-        for (String item : order.keySet()) {
-            System.out.println(item + " " + order.get(item) + "개");
+
+        Map<String, Integer> orderHistory = order.getOrderHistory();
+        for (String item : orderHistory.keySet()) {
+            System.out.println(item + " " + orderHistory.get(item) + "개");
         }
         System.out.println();
     }
 
-    public static void printTotalPriceBeforeDiscount(int price) {
+    public static void printTotalPriceBeforeDiscount(Order order) {
         System.out.println(Constant.PRINT_TOTAL_BEFORE_DISCOUNT);
-        System.out.println(moneyFormat.format(price) + "원");
+        System.out.println(moneyFormat.format(order.getTotalPriceBeforeDisCount()) + "원");
         System.out.println();
     }
 
-    public static void printGift(String gift) {
+    public static void printGift(Order order) {
         System.out.println(Constant.GIFT_MENU);
-        System.out.println(gift);
+
+        if (order.checkGiftTarget()) {
+            System.out.println(Constant.GIFT);
+            System.out.println();
+            return;
+        }
+
+        System.out.println(Constant.NOTHING);
         System.out.println();
     }
 
-    public static void printBenefitHistory(Map<String, Integer> discount) {
+    public static void printBenefitHistory(Discount discount) {
         System.out.println(Constant.BENEFIT_HISTORY);
-        int count = 0;
-        for (String item : discount.keySet()) {
-            if (discount.get(item) != 0) {
-                System.out.println(item + ":" + moneyFormat.format(discount.get(item)) + "원");
-                count += 1;
+        Map<String, Integer> discountHistory = discount.makeBenefitHistory();
+
+        if (discountHistory.size() == 0) {
+            System.out.println(Constant.NOTHING);
+            System.out.println();
+            return;
+        }
+        for (String event : discountHistory.keySet()) {
+            System.out.println(event + ":" + moneyFormat.format(discountHistory.get(event)) + "원");
+        }
+
+        System.out.println();
+    }
+
+    public static void printTotalBenefit(Order order, Discount discount) {
+        System.out.println(Constant.TOTAL_BENEFIT);
+        int amount = discount.getTotalDiscount();
+
+        if (order.checkGiftTarget()) {
+            amount += Food.CHAMPAGNE.getPrice();
+        }
+
+        System.out.println(moneyFormat.format(amount * (-1)) + "원");
+        System.out.println();
+    }
+
+    public static void printExpectedPrice(Order order, Discount discount) {
+        System.out.println(Constant.EXPECTED_PRICE);
+        int amount = order.getTotalPriceBeforeDisCount() - discount.getTotalDiscount();
+
+        System.out.println(moneyFormat.format(amount) + "원");
+        System.out.println();
+    }
+
+    public static void printEventBadge(Order order, Discount discount) {
+        System.out.println(Constant.EVENT_BADGE);
+        int total = discount.getTotalDiscount();
+
+        if (order.checkGiftTarget()) {
+            total += 25000;
+        }
+        for (Badge badge : Badge.values()) {
+            if (total >= badge.getAmount()) {
+                System.out.println(badge.getBadgeName());
+                break;
             }
         }
-
-        if (count == 0) {
-            System.out.println(Constant.NOTHING);
-        }
-        System.out.println();
-    }
-
-    public static void printTotalBenefit(int amount) {
-        System.out.println(Constant.TOTAL_BENEFIT);
-        System.out.println(moneyFormat.format(amount) + "원");
-        System.out.println();
-    }
-
-    public static void printExpectedPrice(int amount) {
-        System.out.println(Constant.EXPECTED_PRICE);
-        System.out.println(moneyFormat.format(amount) + "원");
-        System.out.println();
-    }
-
-    public static void printEventBadge(String badge) {
-        System.out.println(Constant.EVENT_BADGE);
-        System.out.print(badge);
     }
 }
