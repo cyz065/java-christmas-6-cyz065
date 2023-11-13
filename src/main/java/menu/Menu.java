c8a709fb397cca4284c2afd918a3c45c361c03c2
PJH <parkjh0858@gmail.com>
2023-11-13 22:02:24 +0900

    refactor: 불필요한 멤버변수, 메소드 제거

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -1,27 +1,12 @@
 package menu;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Menu {
-
     private static Map<String, String> itemWithCategory;
-
     public Menu() {
-        Map<String, List<String>> menu = setMenuByCategory();
         itemWithCategory = setMenuByItem();
-    }
-
-    // 카테고리별 음식들 나열 ex) 애피타이저 = [양송이수프, 타파스, 시저샐러드]
-    private Map<String, List<String>> setMenuByCategory() {
-        Map<String, List<String>> menu = new HashMap<>();
-
-        for (Food food : Food.values()) {
-            menu.put(food.getCategory(), Food.getFoodsByCategory(food.getCategory()));
-        }
-
-        return menu;
     }
 
     // 음식 별 카테고리 나열 ex) 양송이수프 - 애피타이저, 티본스테이크 - 메인
